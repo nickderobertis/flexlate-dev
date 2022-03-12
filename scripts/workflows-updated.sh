744245f67825c557965c5d534f91987fdf64d531
Nick DeRobertis <whoopnip@gmail.com>
2022-03-12 13:39:44 -0500

    Update issue template and make sure it doesn't get committed

--- scripts/workflows-updated.sh
+++ scripts/workflows-updated.sh
@@ -15,12 +15,17 @@ cat << EOF > temp-issue-template.md;
 title: Manual Update to Files from Copier Needed
 labels: automated issue, maintenance
 ---
-The template from the [Copier which created this project][1] must be updated using Flexlate.
+The template from the [Copier that created this project][1] must be updated using Flexlate.
 
 Normally this is an automated process, but $issue_reason
 
 Run \`pipenv run fxt update -n\` then manually review and update the changes, before pushing a PR
 for this.
+
+```
+fxt check
+$(fxt check)
+```
 
 [1]: https://github.com/nickderobertis/copier-pypi-sphinx-flexlate
 
