4fca556ad9f72a2ebba156fb5632b9b3282d0e6c
Nick DeRobertis <whoopnip@gmail.com>
2022-04-24 14:49:58 -0400

    Add a pre-check command to default publish config that will try to clone the repo if it does not exist locally

--- flexlate_dev/config.py
+++ flexlate_dev/config.py
@@ -45,6 +45,9 @@ class FullRunConfiguration(BaseModel):
 
 def create_default_run_configs() -> Dict[str, UserRootRunConfiguration]:
     default_publish = UserRootRunConfiguration(
+        pre_check=[
+            'if [ -n "$(find . -prune -empty 2>/dev/null)" ]; then gh repo clone {{ data.folder_name }} .; fi'
+        ],
         post_init=[
             "gh repo create --public --source=.",
             "git push origin master",
