49b3badd862f98d0db0a78225984086862e20ce4
Nick DeRobertis <whoopnip@gmail.com>
2022-05-21 11:21:42 -0400

    Remove reliance on base path: treat everything as relative

--- flexlate_dev/ignore.py
+++ flexlate_dev/ignore.py
@@ -1,34 +1,41 @@
-import io
 import tempfile
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List, Callable
 
-from pydantic import BaseModel
 from gitignore_parser import parse_gitignore
 
 
 @dataclass
 class IgnoreSpecification:
     ignore_list: List[str]
-    base_dir: Path
     _ignore_matches: Callable[[str], bool] = field(init=False)
 
     def __post_init__(self):
         self._ignore_matches = self._build_ignore_matches()
 
     def _build_ignore_matches(self) -> Callable[[str], bool]:
-        return parse_gitignore_list_into_matcher(self.ignore_list, self.base_dir)
+        return parse_gitignore_list_into_matcher(self.ignore_list)
 
     def file_is_ignored(self, file_path: str) -> bool:
         return self._ignore_matches(file_path)
 
 
 def parse_gitignore_list_into_matcher(
-    gitignore_list: List[str], base_dir: Path
+    gitignore_list: List[str],
 ) -> Callable[[str], bool]:
     with tempfile.TemporaryDirectory() as temp_dir:
-        temp_path = Path(temp_dir) / ".gitignore"
-        temp_path.write_text("\n".join(gitignore_list) + "\n")
-        matcher = parse_gitignore(temp_path, base_dir=base_dir)
+        temp_path = Path(temp_dir)
+        gitignore_path = temp_path / ".gitignore"
+        gitignore_path.write_text("\n".join(gitignore_list) + "\n")
+        base_matcher = parse_gitignore(gitignore_path, base_dir=temp_dir)
+
+    def matcher(file_path: str) -> bool:
+        """
+        Adjust the base_matcher from gitignore_parser so that relative paths are
+        resolved in the temp directory.
+        """
+        full_path = temp_path / file_path
+        return base_matcher(full_path)
+
     return matcher
--- tests/test_ignore.py
+++ tests/test_ignore.py
@@ -1,16 +1,13 @@
 from flexlate_dev.ignore import IgnoreSpecification
-from tests.config import GENERATED_FILES_DIR
-from tests.fixtures.temp_dir import inside_generated_dir
 
 
-def test_ignore_file(inside_generated_dir):
-    ignore = IgnoreSpecification(ignore_list=["a.txt"], base_dir=GENERATED_FILES_DIR)
+def test_ignore_file():
+    ignore = IgnoreSpecification(ignore_list=["a.txt"])
     assert ignore.file_is_ignored("a.txt")
     assert not ignore.file_is_ignored("b.txt")
 
 
-def test_ignore_folder(inside_generated_dir):
-    ignore = IgnoreSpecification(ignore_list=["a/"], base_dir=GENERATED_FILES_DIR)
-    b_path = GENERATED_FILES_DIR / "a" / "b.txt"
-    assert ignore.file_is_ignored(b_path)
+def test_ignore_folder():
+    ignore = IgnoreSpecification(ignore_list=["a/"])
+    assert ignore.file_is_ignored("a/b.txt")
     assert not ignore.file_is_ignored("b.txt")
