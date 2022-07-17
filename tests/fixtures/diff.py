00ac77c636e9c75b410d031a283ba36c02d0187e
Nick DeRobertis <whoopnip@gmail.com>
2022-07-17 09:41:07 -0400

    chore: Restructure simple diff fixtures

--- tests/fixtures/diff.py
+++ tests/fixtures/diff.py
@@ -14,6 +14,14 @@ def _get_diff_between_commit_and_parent(repo: Repo, sha: str) -> PatchSet:
     return get_diff_between_commits(repo, parent.hexsha, sha)
 
 
+def _get_single_file_diff_between_commit_and_parent(
+    repo: Repo, sha: str
+) -> PatchedFile:
+    diff = _get_diff_between_commit_and_parent(repo, sha)
+    assert len(diff) == 1
+    return diff[0]
+
+
 ADDED_FILE_SHA: Final[str] = "941efdf4533463986e81cfcc62f129ef899bc92b"
 REMOVED_FILE_SHA: Final[str] = "3d3157a424270d295079cd29ce49f6f58b6aa616"
 RENAMED_FILE_SHA: Final[str] = "6d0b0b85b6a64ca02ddb8947c3cfddd1f64ea851"
@@ -22,8 +30,9 @@ MODIFIED_FILE_SHA: Final[str] = "cdc3b85ff0db4d9a38720d9a70010849193e4fdf"
 
 @pytest.fixture(scope="session")
 def file_added_diff(flexlate_dev_repo: Repo) -> PatchedFile:
-    patch_set = _get_diff_between_commit_and_parent(flexlate_dev_repo, ADDED_FILE_SHA)
-    return patch_set[0]
+    return _get_single_file_diff_between_commit_and_parent(
+        flexlate_dev_repo, ADDED_FILE_SHA
+    )
 
 
 @pytest.fixture(scope="session")
@@ -46,7 +55,6 @@ def file_renamed_diff(flexlate_dev_repo: Repo) -> PatchedFile:
 
 @pytest.fixture(scope="session")
 def file_modified_diff(flexlate_dev_repo: Repo) -> PatchedFile:
-    patch_set = _get_diff_between_commit_and_parent(
+    return _get_single_file_diff_between_commit_and_parent(
         flexlate_dev_repo, MODIFIED_FILE_SHA
     )
-    return patch_set[0]
